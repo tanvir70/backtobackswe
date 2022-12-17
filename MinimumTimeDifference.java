3fa7dc41170efbf2b543e01d441937c597e8036f
Tanvir Anjum Rifat <80834028+tanvir70@users.noreply.github.com>
2022-12-17 14:32:05 +0600

    Update MinimumTimeDifference.java

--- MinimumTimeDifference.java
+++ MinimumTimeDifference.java
@@ -1,52 +1,46 @@
 package swe;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 public class MinimumTimeDifference {
     int findMinDifference(List<String> timePoints) {
-        boolean[] time_seen = new boolean[1440];
+        boolean[] timeSeen = new boolean[1440];
 
         for (String time : timePoints) {
-            String[] time_split = time.split(":");
-            int hour = Integer.parseInt(time_split[0]);
-            int minute = Integer.parseInt(time_split[1]);
-            if (time_seen[(hour * 60) + minute]) {
+            String[] timeSplit = time.split(":");
+            int hour = Integer.parseInt(timeSplit[0]);
+            int minute = Integer.parseInt(timeSplit[1]);
+            if (timeSeen[(hour * 60) + minute]) {
                 return 0;
             }
-            time_seen[(hour * 60) + minute] = true;
+            timeSeen[(hour * 60) + minute] = true;
         }
 
-        Integer first_time_seen = null;
-        Integer prev_time_seen = null;
-        int minimum_difference = Integer.MAX_VALUE;
+        Integer firstTimeSeen = null;
+        Integer prevTimeSeen = null;
+        int minimumDifference = Integer.MAX_VALUE;
 
         for (int i = 0; i < 1440; i++) {
-            if (time_seen[i]){
-                if (first_time_seen == null){
-                    first_time_seen = i;
-                    prev_time_seen = i;
-                }else{
-                    minimum_difference = Math.min(minimum_difference, Math.min( (i - prev_time_seen), (1440 - (i + prev_time_seen))));
-                    prev_time_seen = i;
+            if (timeSeen[i]) {
+                if (firstTimeSeen == null) {
+                    firstTimeSeen = i;
+                    prevTimeSeen = i;
+                } else {
+                    minimumDifference = Math.min(minimumDifference, Math.min((i - prevTimeSeen), (1440 - (i + prevTimeSeen))));
+                    prevTimeSeen = i;
                 }
             }
         }
-        minimum_difference = Math.min(minimum_difference, Math.min((prev_time_seen - first_time_seen), 1440  - prev_time_seen + first_time_seen));
+        minimumDifference = Math.min(minimumDifference, Math.min((prevTimeSeen - firstTimeSeen), 1440 - prevTimeSeen + firstTimeSeen));
 
-        return minimum_difference;
+        return minimumDifference;
     }
-
     public static void main(String[] args) {
         var timePoint = new ArrayList<String>();
         timePoint.add("00:03");
         timePoint.add("23:59");
-        timePoint.add("12:03");
-        
         MinimumTimeDifference ob = new MinimumTimeDifference();
         System.out.print("Minimum difference is  : " + ob.findMinDifference(timePoint));
-
-
+        
     }
-
 }
